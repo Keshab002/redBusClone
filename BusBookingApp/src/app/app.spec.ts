5e7f29d790b619205560aa43120e05129e3de31e
Keshab002 <keshavsen002@gmail.com>
2026-01-13 13:03:45 +0530

    added component and service unit testing files

--- BusBookingApp/src/app/app.spec.ts
+++ BusBookingApp/src/app/app.spec.ts
@@ -1,10 +1,26 @@
 import { TestBed } from '@angular/core/testing';
 import { App } from './app';
+import { of } from 'rxjs';
+import { Master } from './service/master';
+import { APP_SERVICE_CONFIG } from './config/appConfig.service';
+
+let MockMasterService = {
+  loginUser: vi.fn().mockReturnValue(of({ data: {}, message: '' })),
+  registerUser: vi.fn().mockReturnValue(of({ data: {}, message: '' })),
+};
+
+const mockConfig = {
+  apiUrl: 'https://mockapi.example.com',
+};
 
 describe('App', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [App],
+      providers: [
+        { provide: Master, useValue: MockMasterService },
+        { provide: APP_SERVICE_CONFIG, useValue: mockConfig },
+      ],
     }).compileComponents();
   });
 
@@ -14,10 +30,79 @@ describe('App', () => {
     expect(app).toBeTruthy();
   });
 
-  it('should render title', async () => {
+  it('should open Modal', () => {
     const fixture = TestBed.createComponent(App);
-    await fixture.whenStable();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, BusBookingApp');
+    const app = fixture.componentInstance;
+    app.openModal();
+    expect(app.isOpen).toBeTruthy();
+  });
+
+  it('should close Modal', () => {
+    const fixture = TestBed.createComponent(App);
+    const app = fixture.componentInstance;
+    app.closeModal();
+    expect(app.isOpen).toBeFalsy();
+  });
+
+  it('should toggle isLogin State', () => {
+    const fixture = TestBed.createComponent(App);
+    const app = fixture.componentInstance;
+    const initialState = app.isLogin;
+    app.toggleLogin();
+    expect(app.isLogin).toBe(!initialState);
+  });
+
+  it('should call registerUser from Master service on registerUser method call', () => {
+    const fixture = TestBed.createComponent(App);
+    const app = fixture.componentInstance;
+    app.onRegister({ reset: vi.fn() });
+    expect(MockMasterService.registerUser).toHaveBeenCalled();
+  });
+
+  it('should call loginUser from Master service on loginUser method call', () => {
+    const fixture = TestBed.createComponent(App);
+    const app = fixture.componentInstance;
+    app.onLogin({ reset: vi.fn() });
+    expect(MockMasterService.loginUser).toHaveBeenCalled();
+  });
+
+  it('should logout user', () => {
+    const fixture = TestBed.createComponent(App);
+    const app = fixture.componentInstance;
+    localStorage.setItem('RedBusUser', JSON.stringify({ name: 'Test User' }));
+    app.onLogout();
+    expect(app.loggedinUser).toBeNull();
+    expect(localStorage.getItem('RedBusUser')).toBeNull();
+  });
+
+  it('should initialize loggedinUser from localStorage if present', () => {
+    const userData = { name: 'Test User' };
+    localStorage.setItem('RedBusUser', JSON.stringify(userData));
+    const fixture = TestBed.createComponent(App);
+    fixture.detectChanges();
+    const app = fixture.componentInstance;
+    expect(app.loggedinUser).toEqual(userData);
+    expect(app.isOpen).toBeFalsy();
+  });
+
+  it('should called onLogin on login form submit', () => {
+    const fixture = TestBed.createComponent(App);
+    const app = fixture.componentInstance;
+    app.isOpen = true;
+    app.isLogin = true;
+    fixture.detectChanges();
+
+    const buttons = Array.from(
+      fixture.nativeElement.querySelectorAll('button')
+    ) as HTMLButtonElement[];
+
+    const signInButton = buttons.find((btn) => btn.textContent?.trim() === 'Sign in');
+
+    const loginSpy = vi.spyOn(app, 'onLogin');
+
+    expect(signInButton).toBeTruthy();
+    signInButton?.click();
+    fixture.detectChanges();
+    expect(loginSpy).toHaveBeenCalled();
   });
 });
--- BusBookingApp/src/app/service/master.spec.ts
+++ BusBookingApp/src/app/service/master.spec.ts
@@ -1,16 +1,131 @@
 import { TestBed } from '@angular/core/testing';
 
 import { Master } from './master';
+import { APP_SERVICE_CONFIG } from '../config/appConfig.service';
+import { HttpTestingController, provideHttpClientTesting } from '@angular/common/http/testing';
 
+const mockConfig = {
+  apiUrl: 'https://mockapi.example.com',
+};
+
+// What does describe do in testing frameworks like Jasmine or Mocha?
+// it is used to group related tests together, providing a way to organize and structure test cases for better readability and maintainability.
 describe('Master', () => {
   let service: Master;
+  let httpMock: HttpTestingController;
 
+  // What is the purpose of beforeEach in testing frameworks?
+  // beforeEach is used to set up a specific state or configuration before each test case runs, ensuring that each test starts with a clean and consistent environment.
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        Master,
+        {
+          provide: APP_SERVICE_CONFIG,
+          useValue: mockConfig,
+        },
+        provideHttpClientTesting(),
+      ],
+    });
     service = TestBed.inject(Master);
+    httpMock = TestBed.inject(HttpTestingController);
   });
 
+  // What does it do in testing frameworks like Jasmine or Mocha?
+  // it defines an individual test case, specifying the expected behavior or outcome that should be verified.
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should fetch bus locations', () => {
+    const mockResponse = { message: '', data: [] };
+    service.getLocation().subscribe((res) => {
+      expect(res).toEqual(mockResponse);
+    });
+    const req = httpMock.expectOne(`${mockConfig.apiUrl}/GetBusLocations`);
+    expect(req.request.method).toBe('GET');
+    req.flush(mockResponse);
+  });
+
+  it('should search buses', () => {
+    const mockResponse = { message: '', data: [] };
+    let locationId = 1;
+    let toLocationId = 2;
+    let scheduleDate = '2024-01-01';
+
+    service.searchBuses(locationId, toLocationId, scheduleDate).subscribe((res) => {
+      expect(res).toEqual(mockResponse);
+    });
+    const req = httpMock.expectOne(
+      `${mockConfig.apiUrl}/SearchBuses?fromLocationId=${locationId}&toLocationId=${toLocationId}&scheduleDate=${scheduleDate}`
+    );
+    expect(req.request.method).toBe('GET');
+    req.flush(mockResponse);
+  });
+
+  it('should get bus by schedule ID', () => {
+    const mockResponse = { message: '', data: {} };
+    let scheduleId = 1;
+
+    service.getBusbyScehduleId(scheduleId).subscribe((res) => {
+      expect(res).toEqual(mockResponse);
+    });
+    const req = httpMock.expectOne(`${mockConfig.apiUrl}/GetSpecificBus?scheduleId=${scheduleId}`);
+    expect(req.request.method).toBe('GET');
+    req.flush(mockResponse);
+  });
+
+  it('should get booked seats', () => {
+    const mockResponse = { message: '', data: [] };
+    let scheduleId = 1;
+
+    service.getBookedSeats(scheduleId).subscribe((res) => {
+      expect(res).toEqual(mockResponse);
+    });
+    const req = httpMock.expectOne(`${mockConfig.apiUrl}/GetBookedSeats?scheduleId=${scheduleId}`);
+    expect(req.request.method).toBe('GET');
+    req.flush(mockResponse);
+  });
+
+  it('should register user', () => {
+    const mockResponse = { message: '', data: {} };
+    const registerObj = { username: 'test', password: 'test123' };
+
+    service.registerUser(registerObj).subscribe((res) => {
+      expect(res).toEqual(mockResponse);
+    });
+    const req = httpMock.expectOne(`${mockConfig.apiUrl}/register`);
+    expect(req.request.method).toBe('POST');
+    req.flush(mockResponse);
+  });
+
+  it('should login user', () => {
+    const mockResponse = { message: '', data: {} };
+    const loginObj = { username: 'test', password: 'test123' };
+
+    service.loginUser(loginObj).subscribe((res) => {
+      expect(res).toEqual(mockResponse);
+    });
+    const req = httpMock.expectOne(`${mockConfig.apiUrl}/login`);
+    expect(req.request.method).toBe('POST');
+    req.flush(mockResponse);
+  });
+
+  it('should post booking', () => {
+    const mockResponse = { message: '', data: {} };
+    const bookingObj = { scheduleId: 1, seats: [1, 2, 3] };
+    service.postBooking(bookingObj).subscribe((res) => {
+      expect(res).toEqual(mockResponse);
+    });
+    const req = httpMock.expectOne(`${mockConfig.apiUrl}/BookSeats`);
+    expect(req.request.method).toBe('POST');
+    req.flush(mockResponse);
+  });
+
+  // What is the purpose of afterEach in testing frameworks?
+  // afterEach is used to clean up or reset the state after each test case runs, ensuring that no residual effects from one test affect subsequent tests.
+  afterEach(() => {
+    // Verify that there are no outstanding HTTP requests
+    httpMock.verify();
   });
 });
